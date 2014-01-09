Desocrit/Scripting

commit 2fe4fe541cf21e726c02a8f4c6e4ece34d07a73f

Better use of HTMLParser()

server.py
@@ -285,7 +285,7 @@ class MainPage(webapp2.RequestHandler):
     def add_css(self, url):
         ''' Reads a css file, adding to or getting from the database. '''
         try:
-            css = urllib.urlopen(HTMLParser.HTMLParser().unescape(url)).read()
+            css = urllib.urlopen(html.unescape(url)).read()
         except:
             return False
 
@@ -794,6 +794,7 @@ class MainPage(webapp2.RequestHandler):
         ''' Draws the main page, and handles any commands '''
         self.output_type = self.request.get('output_type', DEFAULT_OUTPUT_TYPE)
         self.json = {}
+        self.html = HTMLParser.HTMLParser()
 
         # This is only required for the dev pages, and they don't need
         # to be compliant. But it was printing this out at inappropriate

commit 243890aaa3e8ec4ec5da5b28ddff0b5bb056d4d1

Fixed CSS Bug introduced by 2fe4fe5

server.py
@@ -285,7 +285,7 @@ class MainPage(webapp2.RequestHandler):
     def add_css(self, url):
         ''' Reads a css file, adding to or getting from the database. '''
         try:
-            css = urllib2.urlopen(html.unescape(url)).read()
+            css = urllib2.urlopen(self.html.unescape(url)).read()
         except:
             return False
 